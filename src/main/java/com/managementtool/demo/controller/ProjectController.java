f761de7d9bd8daba6109d0f20f134a34c0a47025
abdulahi roble <abdulahi_2610@hotmail.com>
2020-12-09 12:34:11 +0100

    Project title

--- src/main/java/com/managementtool/demo/controller/ProjectController.java
+++ src/main/java/com/managementtool/demo/controller/ProjectController.java
@@ -2,17 +2,22 @@ package com.managementtool.demo.controller;
 
 import com.managementtool.demo.models.Manager;
 import com.managementtool.demo.models.Project;
+import com.managementtool.demo.services.ProjectService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class ProjectController {
     Project project = new Project();
+    ProjectService projectService = new ProjectService();
 
     @GetMapping("/createProject")
     public String createProject(Model projectModel) {
@@ -21,21 +26,29 @@ public class ProjectController {
         return "createProject";
     }
 
-    @PostMapping("/postCreateAccount")
+    @GetMapping("/myprojects")
+    public String showProject(Model model)
+    {
+        List<Project> myproject = new ProjectService().getAllProjects();
+        model.addAttribute(" myproject",myproject);
+
+        return "myprojects";
+    }
+
+    @PostMapping("/postCreateProject")
     public String postCreate(WebRequest dataFromForm, HttpServletResponse response) {
 
         try {
             String projectname = dataFromForm.getParameter("projectname");
-            String category = dataFromForm.getParameter("category");
 
-            Project project = new Project(projectname, category);
+            Project project = new Project(projectname);
 
-            // managerService.insertNewManager(manager);
+             projectService.insertNewProject(project);
         } catch (Exception e) {
             System.out.println("Fejl:" + e);
 
         }
 
-        return "home";
+        return "redirect:/projects";
     }
 }
